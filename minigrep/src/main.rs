7749315a19251dc56db5e625fab69f363f2fda72
hamittopurple <happypurpleblob@gmail.com>
2023-12-28 21:28:47 +0800

    feat: chapter 12 - add function to read file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // Read command line arguments
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
-    let filepath = &args[2];
+    let file_path = &args[2];
 
     println!("Searching for {query}");
-    println!("In file {filepath}");
+    println!("In file {file_path}");
+
+    // Read contents of file_path
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("With text:\n{contents}")
 }
