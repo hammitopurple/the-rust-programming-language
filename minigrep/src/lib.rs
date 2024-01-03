087977f87d5faec8153e0455ee8b6248bad2ab39
hamittopurple <happypurpleblob@gmail.com>
2024-01-03 20:00:08 +0800

    feat(chp12): integrate run with main

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -5,9 +5,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read contents of file_path
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    // Iterate through each line of contents
+    for line in contents.lines() {
+        // Check if any line matches
+        if line.contains(query) {
+            // If it does, add to the list of values we are returning
+            results.push(line);
+        }
+    }
+    // Return the list of results that match
+    results
 }
 
 pub struct Config {
@@ -25,5 +42,21 @@ impl Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
