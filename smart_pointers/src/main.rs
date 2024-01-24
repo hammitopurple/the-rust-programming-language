b176bc7e91fcd1f69880b9901f8f03b07d8eefa4
hamittopurple <happypurpleblob@gmail.com>
2024-01-24 23:28:26 +0800

    feat(chp15): add code for 15.2

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -17,7 +17,49 @@ fn create_box() {
     println!("b = {}", *b); // manual dereference
 }
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn deref() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+fn hello_too(name: &String) {
+    println!("Hello too, {name}!");
+}
+
 fn main() {
     create_box();
     create_list();
+
+    deref();
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m); // 2 applications of deref coercion: &MyBox to &String, then &String to &str
+    hello_too(&m); // vanilla case 1 of deref coercion, &T to &U
+
+    let b = Box::new(String::from("rust"));
+    hello(&b);
 }
