68fe2f80f53344c4b0af2d58682e3a65c90c27f5
hamittopurple <happypurpleblob@gmail.com>
2024-03-11 22:47:19 +0800

    feat(chp20): add graceful shutdown for 20.3

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -10,7 +10,7 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
@@ -36,7 +36,10 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -46,13 +49,33 @@ impl ThreadPool {
         // Box up the function so that we can send it to a Worker that holds a receiver
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        // We use `as_ref` here because we are not trying to take ownership of `sender` from
+        // the ThreadPool value
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // drop sender first before anything dealing with the workers
+        // note that we are calling `take` because we need ownership of the value
+        // before we can call `drop` on it
+        drop(self.sender.take());
+
+        // Iterate through all the workers and joining them
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -62,13 +85,23 @@ impl Worker {
         let thread = thread::spawn(move || loop {
             // receiver.lock() works because Arc implements the Deref trait
             // A Mutex needs to call lock() first before it can use the value inside
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing.");
-
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break; // this ends the infinite loop and terminates the thread closure
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
